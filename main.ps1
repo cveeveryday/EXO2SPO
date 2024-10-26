cd795bd516e0136d1b409031e0efeba7755abab4
CVE Everyday <cveeveryday@outlook.com>
2024-10-25 22:52:08 -0400

    execution statement udpates

--- main.ps1
+++ main.ps1
@@ -5,20 +5,35 @@ Import-Module ..\SPFunctions\spofunctions.psm1
 
 $token = Get-GraphToken -appID $appID -clientSecret $clientSecret -tenantID $tenantID
 <#
-New-SPListFromObject -token $token -siteName "Team Site" -listName "Patty's Emails" #-colunmns @("From","To","DateReceived","Subject","Body","isRead") 
-Update-SPListColumnName -token $token -siteName "Team Site" -listName "PattysEmails" -OldColumnName "Title" -NewColumnName "MessageId"
-Add-SPListColumn  -token $token  -siteName "Team Site"  -listName "PattysEmails"  -ColumnName "Subject"  -ColumnType "Text"
-Add-SPListColumn -token $token -siteName "Team Site" -listName "PattysEmails" -ColumnName "Sender" -ColumnType "Text"
-Add-SPListColumn -token $token -siteName "Team Site" -listName "PattysEmails" -ColumnName "To" -ColumnType "Text"
-Add-SPListColumn -token $token -siteName "Team Site" -listName "PattysEmails" -ColumnName "Body" -ColumnType "multilineText"
-Add-SPListColumn  -token $token  -siteName "Team Site"  -listName "PattysEmails"  -ColumnName "DateReceived"  -ColumnType "dateTime"
-Add-SPListColumn -token $token  -siteName "Team Site"  -listName "PattysEmails"  -ColumnName "Status"  -ColumnType "Text"
+New-SPListFromObject -token $token -siteName "afterhours" -listName "Patty's Emails" #-colunmns @("From","To","DateReceived","Subject","Body","isRead") 
+Update-SPListColumnName -token $token -siteName "afterhours" -listName "PattysEmails" -OldColumnName "Title" -NewColumnName "MessageId"
+Add-SPListColumn  -token $token  -siteName "afterhours"  -listName "PattysEmails"  -ColumnName "Subject"  -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "PattysEmails" -ColumnName "Sender" -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "PattysEmails" -ColumnName "To" -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "PattysEmails" -ColumnName "Body" -ColumnType "multilineText"
+Add-SPListColumn  -token $token  -siteName "afterhours"  -listName "PattysEmails"  -ColumnName "DateReceived"  -ColumnType "dateTime"
+Add-SPListColumn -token $token  -siteName "afterhours"  -listName "PattysEmails"  -ColumnName "Status"  -ColumnType "Text"
+
+$list = Get-SPLists  -token $token  -siteName "afterhours"  -listName "PattysEmails"
+#$items = Get-SPListItems -accessToken $token -siteName "afterhours" -listName "PattysEmails" -ColumnName "Status" -filter "In Progress"
 #>
-$list = Get-SPLists  -token $token  -siteName "Team Site"  -listName "PattysEmails"
-#$items = Get-SPListItems -accessToken $token -siteName "Team Site" -listName "PattysEmails" -ColumnName "Status" -filter "In Progress"
+
+New-SPListFromObject -token $token -siteName "afterhours" -listName "staffEdmonton" #-colunmns @("From","To","DateReceived","Subject","Body","isRead") 
+Update-SPListColumnName -token $token -siteName "afterhours" -listName "staffEdmonton" -OldColumnName "Title" -NewColumnName "Threshold"
+Add-SPListColumn  -token $token  -siteName "afterhours"  -listName "staffEdmonton" -ColumnName "Frequency" -ColumnType "Number"
+Add-SPListColumn  -token $token  -siteName "afterhours"  -listName "staffEdmonton" -ColumnName "FirstName" -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "staffEdmonton" -ColumnName "LastName" -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "staffEdmonton" -ColumnName "PhoneNumber" -ColumnType "Text"
+Add-SPListColumn -token $token -siteName "afterhours" -listName "staffEdmonton" -ColumnName "Enabled" -ColumnType "Boolean"
+
+$message = Get-OldestVoiceMailMessage -accessToken $token -emailAddress "PattiF@zpzbx.onmicrosoft.com"
+$message 
+
+<#
 $folderId = (Get-MailFolder -accessToken $token -emailAddress "PattiF@zpzbx.onmicrosoft.com" -folderName "Inbox").id
-$emailsAll = Get-MailMessages -accessToken $token -emailAddress "PattiF@zpzbx.onmicrosoft.com"  -folderId $folderId -limit 10
+$emailsAll = Get-MailMessages -accessToken $token -emailAddress "PattiF@zpzbx.onmicrosoft.com"  -folderId $folderId -limit 199
 $emailsProcessed = @()
+$emailsStaged = @()
 foreach ($email in $emailsAll) {
   $emailsProcessed += [PSCustomObject]@{
     Subject = ConvertFrom-Html $email.subject
@@ -31,16 +46,17 @@ foreach ($email in $emailsAll) {
   }
 }
 
-Add-ObjectToSPList -token $token -siteName "Team Site" -listName "PattysEmails" -objects $emailsProcessed
+Add-ObjectToSPList -token $token -siteName "afterhours" -listName "PattysEmails" -objects $emailsProcessed
 
-$emailsStaged = Get-SPListItems -accessToken $token   -siteName "Team Site"  -listName "PattysEmails"   -ColumnName "Status"  -filter "Staged"
+$emailsStaged = Get-SPListItems -accessToken $token   -siteName "afterhours"  -listName "PattysEmails" -ColumnName "Status"  -filter "Staged"
 
 foreach ($email in $emailsStaged) {
-  $email = Get-SPListItem -accessToken $token -siteName "Team Site" -listName "PattysEmails" -itemId $email.ID
+  $email = Get-SPListItem -accessToken $token -siteName "afterhours" -listName "PattysEmails" -itemId $email.ID
   $newMessageId = Move-MailMessage -accessToken $token -emailAddress "PattiF@zpzbx.onmicrosoft.com" -folderName "In Progress" -messageId $email.fields.Title
   If ($newMessageId)
   {
-    $update = Update-SPListItems -accessToken $token -siteName "Team Site" -listName "PattysEmails" -ColumnName "Title" -filter $email.fields.Title -newValue $newMessageId
+    $result = Update-SPListItems -accessToken $token -siteName "afterhours" -listName "PattysEmails" -ColumnNameFilter "Title" -FilterValue $email.fields.Title  -ColumnName "Title" -newValue $newMessageId
+    $result = Update-SPListItems -accessToken $token -siteName "afterhours" -listName "PattysEmails" -ColumnNameFilter "Title" -FilterValue $email.fields.Title  -ColumnName "Status" -newValue "Processed"
   }
   $update
 }
